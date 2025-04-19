690f59a2128c6e18dc1ab404f60428b90407fdb3
brandonjinkim <brandonjinkim@gmail.com>
2025-04-18 17:39:12 -0700

    Added coupon functionality and added 'discount' tab on receipt to show if an item was discounted or now (works for two for one as well)

--- Store/Store/main.swift
+++ Store/Store/main.swift
@@ -10,15 +10,18 @@ import Foundation
 protocol SKU {
     var name : String { get }
     func price() -> Int
+    var itemDiscount: Bool { get }
 }
 
 class Item : SKU {
     let name: String
     let priceEach: Int
+    let itemDiscount: Bool
     
-    init(name: String, priceEach: Int) {
+    init(name: String, priceEach: Int, itemDiscount: Bool = false) {
         self.name = name
         self.priceEach = priceEach
+        self.itemDiscount = itemDiscount
     }
     
     func price() -> Int {
@@ -30,15 +33,20 @@ class ItemByWeight : SKU {
     let name: String
     let weight: Double
     let pricePerPound: Int
+    let itemDiscount: Bool
+    var priceEach: Int
     
-    init(name: String, weight: Double, pricePerPound: Int) {
+    init(name: String, weight: Double, pricePerPound: Int, itemDiscount: Bool = false, priceEach: Int = 0) {
         self.name = name
         self.weight = weight
         self.pricePerPound = pricePerPound
+        self.itemDiscount = itemDiscount
+        self.priceEach = priceEach
     }
     
     func price() -> Int {
-        return Int(Double(pricePerPound) * weight)
+        priceEach = Int(Double(pricePerPound) * weight)
+        return priceEach
     }
     
 }
@@ -62,7 +70,7 @@ class Receipt {
             let priceDollars = Double(item.price()) / 100.0
             
             //"%.2f" makes the double 2 decimal points, even in the case that it equals 0
-            output += "\(item.name): $\(String(format: "%.2f", priceDollars))\n"
+            output += "\(item.name): $\(String(format: "%.2f", priceDollars)) Discounted: \(item.itemDiscount ? "Yes" : "No")\n"
         }
         output += "------------------\n"
         let totalDollars = Double(total()) / 100.0
@@ -103,7 +111,7 @@ class Register : PricingScheme {
             // if there are 3 of the same item
             if itemCount[sku.name]! % 3 == 0 {
                 // add item to receipt with price set as 0
-                receipt.add(Item(name: sku.name, priceEach: 0))
+                receipt.add(Item(name: sku.name, priceEach: 0, itemDiscount: true))
             }
             // if not
             else {
@@ -126,6 +134,22 @@ class Register : PricingScheme {
         return totalReceipt
     }
     
+}
+
+class Coupon {
+    func applyDiscount(_ receipt: Receipt, _ name: String, _ discount: Double) {
+        var i = 0
+        while i < receipt.itemList.count {
+            let item = receipt.itemList[i]
+            if (item.name == name && item.itemDiscount == false) {
+                let newPrice = Int(Double(item.price()) * (1.0 - discount))
+                let discountedItem = Item(name: item.name, priceEach: newPrice, itemDiscount: true)
+                receipt.itemList[i] = discountedItem
+                break
+            }
+            i = i + 1
+        }
+    }
 }
 
 class Store {
--- Store/StoreTests/StoreTests.swift
+++ Store/StoreTests/StoreTests.swift
@@ -31,7 +31,7 @@ final class StoreTests: XCTestCase {
 
         let expectedReceipt = """
             Receipt:
-            Beans (8oz Can): $1.99
+            Beans (8oz Can): $1.99 Discounted: No
             ------------------
             TOTAL: $1.99
             """
@@ -58,9 +58,9 @@ final class StoreTests: XCTestCase {
 
         let expectedReceipt = """
             Receipt:
-            Beans (8oz Can): $1.99
-            Pencil: $0.99
-            Granols Bars (Box, 8ct): $4.99
+            Beans (8oz Can): $1.99 Discounted: No
+            Pencil: $0.99 Discounted: No
+            Granols Bars (Box, 8ct): $4.99 Discounted: No
             ------------------
             TOTAL: $7.97
             """
@@ -82,9 +82,9 @@ final class StoreTests: XCTestCase {
         
         let expectedReceipt = """
             Receipt:
-            Beans (8oz Can): $1.99
-            Beans (8oz Can): $1.99
-            Beans (8oz Can): $0.00
+            Beans (8oz Can): $1.99 Discounted: No
+            Beans (8oz Can): $1.99 Discounted: No
+            Beans (8oz Can): $0.00 Discounted: Yes
             ------------------
             TOTAL: $3.98
             """
@@ -104,7 +104,7 @@ final class StoreTests: XCTestCase {
         let receipt = register.total()
         let expectedReceipt = """
             Receipt:
-            Steak: $10.78
+            Steak: $10.78 Discounted: No
             ------------------
             TOTAL: $10.78
             """
@@ -122,11 +122,60 @@ final class StoreTests: XCTestCase {
         let receipt = register.total()
         let expectedReceipt = """
             Receipt:
-            Steak: $10.78
-            Pencil: $0.99
+            Steak: $10.78 Discounted: No
+            Pencil: $0.99 Discounted: No
             ------------------
             TOTAL: $11.77
             """
         XCTAssertEqual(receipt.output(), expectedReceipt)
+    }
+    
+    func testApplyDiscountRegularItem() {
+        let coupon = Coupon()
+        
+        register.scan(Item(name: "Beans (8oz Can)", priceEach: 199))
+        
+        coupon.applyDiscount(register.receipt, "Beans (8oz Can)", 0.15)
+        
+        XCTAssertEqual(register.subtotal(), Int(Double(199) * 0.85))
+    }
+    
+    func testApplyDiscountWeightItem() {
+        let coupon = Coupon()
+        
+        register.scan(ItemByWeight(name: "Steak", weight: 1.2, pricePerPound: 899))
+        
+        coupon.applyDiscount(register.receipt, "Steak", 0.15)
+        
+        XCTAssertEqual(register.subtotal(), Int(Double(899) * 1.2 * 0.85))
+    }
+    
+    func testApplyDiscountMultipleItems() {
+        let coupon = Coupon()
+        
+        register.scan(Item(name: "Beans (8oz Can)", priceEach: 199))
+        register.scan(Item(name: "Beans (8oz Can)", priceEach: 199))
+        register.scan(ItemByWeight(name: "Steak", weight: 1.2, pricePerPound: 899))
+        register.scan(ItemByWeight(name: "Steak", weight: 1.2, pricePerPound: 899))
+        
+        coupon.applyDiscount(register.receipt, "Beans (8oz Can)", 0.15)
+        coupon.applyDiscount(register.receipt, "Beans (8oz Can)", 0.15)
+        coupon.applyDiscount(register.receipt, "Steak", 0.15)
+    
+        let subtotal = register.subtotal()
+        let subtotalTest = Int(Double(199) * 0.85) + Int(Double(199) * 0.85) + Int(Double(899) * 1.2 * 0.85) + Int(Double(899) * 1.2)
+        XCTAssertEqual(subtotal, subtotalTest)
+        
+        let receipt = register.total()
+        let expectedReceipt = """
+            Receipt:
+            Beans (8oz Can): $1.69 Discounted: Yes
+            Beans (8oz Can): $1.69 Discounted: Yes
+            Steak: $9.16 Discounted: Yes
+            Steak: $10.78 Discounted: No
+            ------------------
+            TOTAL: $23.32
+            """
+        XCTAssertEqual(expectedReceipt, receipt.output())
     }
 }
